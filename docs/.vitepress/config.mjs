1bb12ce4bc2f247e005cb6f22e011d192fefd127
Dots <dotvnl@gmail.com>
2024-07-16 21:36:55 +0300

    options

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,6 +1,15 @@
 import { SearchPlugin } from "vitepress-plugin-search";
 import { defineConfig } from "vitepress";
 
+var options = {
+  ...flexSearchIndexOptions,
+  previewLength: 62,
+  buttonLabel: "Search",
+  placeholder: "Search docs",
+  allow: [],
+  ignore: [],
+};
+
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   vite: { plugins: [SearchPlugin(options)] },
